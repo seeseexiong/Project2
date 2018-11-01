1529efaa84610f5e604e2e83d13315ad59fe5404
kkasperek <kt.kasperek@gmail.com>
2018-11-01 12:35:13 -0500

    html routes for login and signup pages

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
+/* //------------------------------------------------------------
+  SERVER.JS ----> Setup 
+*/ //-------------------------------------------------------------
+
+// Require ======================================================
 require("dotenv").config();
-var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
 
 var db = require("./models");
 
+// Express
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Middleware
+// Passport 
+const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
+// Middleware Config ======================================================
+
+// require('./config/passport')(passport); // pass passport for configuration
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser()); // read cookies (needed for auth)
 app.use(express.static("public"));
 
 // Handlebars
@@ -22,9 +38,15 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// Routes ======================================================
+require("./routes/apiRoutes")(app, passport);
+require("./routes/htmlRoutes")(app, passport);
 
 var syncOptions = { force: false };
 
@@ -34,7 +56,7 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
+// Starting the server, syncing our models ==========================================
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
@@ -45,4 +67,4 @@ db.sequelize.sync(syncOptions).then(function() {
   });
 });
 
-module.exports = app;
+//module.exports = app;
