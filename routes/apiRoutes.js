057c41d48258c50456d25166dfebf6e8a94cfe73
DENIS PLASTER <denjeo@gmail.com>
2018-11-02 14:13:19 -0500

    homepage rework

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,24 +3,36 @@ const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('cd587386616044c48131745138aa4aa0');
 
 
+
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/searchUsers", function(req, res) {
+    db.users.findAll({where : {name: req.params.name}}).then(function(dbUsers) {
+      res.json(dbUsers);
+    });
+  });
+
+  app.get("/api/followedPosts", function(req, res) {
+    db.Posts.findAll({where: {post: req.params.Post}}).then(function(dbPosts) {
+      res.json(dbPosts);
+    });
+  });
+  app.get("/api/followedLikes", function(req, res) {
+    db.users.findAll({like: req.params.like}).then(function(dbLikes) {
+      res.json(dbLikes);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/comment", function(req, res) {
+    db.Comments.create(req.body).then(function(dbComment) {
+      res.json(dbComment);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/users/:id", function(req, res) {
+    db.users.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
@@ -29,6 +41,24 @@ module.exports = function(app) {
     newsapi.v2
       .topHeadlines({
         category: /*Favorites variable here*/'business',
+        language: 'en',
+        country: 'us',
+      })
+      .then(response => {
+        console.log(response);
+        res.json(response);
+        /*
+        {
+          status: "ok",
+          articles: [...]
+        }
+      */
+      });
+  });
+  app.get('/api/searchHeadlines', function(req, res) {
+    newsapi.v2
+      .topHeadlines({
+        category: /*Search term */'business',
         language: 'en',
         country: 'us',
       })
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,50 +11,6 @@ module.exports = function (app) {
     // });
   });
 
-  // Main LogIn Page ========================================================
-  app.get('/login/main', function (req, res) {
-    // render the page and pass in any flash data if it exists
-    res.render("login-main", { message: req.flash('loginMessage') });
-  });
-
-  // process the login form
-  // app.post('/login', do all our passport stuff here);
-
-  // Current Users Login ========================================================
-  app.get('/login', function (req, res) {
-    // render the page and pass in any flash data if it exists
-    res.render("login", { message: req.flash('loginMessage') });
-  });
-
-  // process the login form
-  // app.post('/login', do all our passport stuff here);
-
-  // SignUp Page ========================================================
-  app.get('/signup', function (req, res) {
-
-    // render the page and pass in any flash data if it exists
-    res.render("signup", { message: req.flash('signupMessage') });
-  });
-
-  // process the signup form
-  // app.post('/signup', do all our passport stuff here);
-
-  // Profile Page ========================================================
-  // want protected so you have to be logged in to visit
-  // we will use route middleware to verify this (the isLoggedIn function)
-  app.get('/profile', isLoggedIn, function (req, res) {
-    res.render('profile.handlebars', {
-      user: req.user // get the user out of session and pass to template
-    });
-  });
-
-  // LogOut Page ========================================================
-  app.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect('/');
-  });
-
-
   // Load example page and pass in an example by id
   app.get("/example/:id", function (req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
@@ -70,14 +26,3 @@ module.exports = function (app) {
   });
 
 };
-
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-  // if user is authenticated in the session, carry on 
-  if (req.isAuthenticated())
-      return next();
-
-  // if they aren't redirect them to the home page
-  res.redirect('/');
-}
