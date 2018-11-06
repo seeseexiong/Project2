654397dda88bb1c3949233d1ba1e35f5f3129a0d
isaiah <isaiah.ramsey18@gmail.com>
2018-11-06 14:25:13 -0600

    stash

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,13 +5,39 @@ const express = require("express");
 const app = express();
 
 
-module.exports =  (app) => {
-  // Load index page ========================================================
-  app.get('/', function(req, res) {
-    console.log("hi")
-    res.sendFile((path.join(__dirname, "../public/index.html")));
+module.exports = function (app) {
+  // Load Main page ========================================================
+
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
   });
 
+  // app.get("/", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../index.html"));
+  // });
+
+  // Main LogIn Page ========================================================
+  app.get('/login/main',  (req, res) => {
+    // render the page and pass in any flash data if it exists
+    res.render("login-main" );
+  });
+  // cms route loads cms.html
+  app.get("/cms", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  });
+
+
+ // Render 404 page for any unmatched routes
+  app.get("*",  (req, res) => {
+
+  // process the login form
+  // app.post('/login', do all our passport stuff here);
+
+  // Current Users Login ========================================================
+  app.get('/login', function (req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render("login" );
+  });
 
   
   // Main Blog Page ========================================================
@@ -24,9 +50,14 @@ module.exports =  (app) => {
     res.sendFile(path.join(__dirname, "../friend-followers2.html"));
   });
 
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function (req, res) {
+
  // Render 404 page for any unmatched routes
   app.get("*",  (req, res) => {
+
     res.render("404");
   });
-}
-// comment out non working stuff
+})
+  })}
