1b02b58d79c722b1451d2d91be630b53d1758e59
See Xiong <seeseexiong@gmail.com>
2018-11-06 18:02:17 -0600

    up to date with Katie's passport

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
       host: "localhost",
-      port: 3306,
+      port: 8889,
       user: "root",
       password: "root",
       database: "social_storydb"
